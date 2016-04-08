1f743dbf38d438ec67af64fc02739c3340859a64
Richard Jackson <k1223140@kcl.ac.uk>
2016-04-08 15:58:12 +0100

    updated licence

--- src/main/java/uk/ac/kcl/batch/MasterIntegrationConfiguration.java
+++ src/main/java/uk/ac/kcl/batch/MasterIntegrationConfiguration.java
@@ -1,8 +1,20 @@
+/* 
+ * Copyright 2016 King's College London, Richard Jackson <richgjackson@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.kcl.batch;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -19,11 +31,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
-import uk.ac.kcl.model.BinaryDocument;
-import uk.ac.kcl.rowmappers.DocumentMetadataRowMapper;
 import uk.ac.kcl.scheduling.Scheduler;
 
 /**
