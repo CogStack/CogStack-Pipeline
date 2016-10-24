a7b12e41eef250841ac145c6e3ed4f6d14b701fd
Kenneth Lui <k.lui@ucl.ac.uk>
2016-10-24 16:04:52 +0100

    PDFFileItemWriter for MS Word documents (logic from timeline-preprocess project)

--- src/main/java/uk/ac/kcl/itemHandlers/PDFFileItemWriter.java
+++ src/main/java/uk/ac/kcl/itemHandlers/PDFFileItemWriter.java
@@ -2,6 +2,7 @@ package uk.ac.kcl.itemHandlers;
 
 
 import org.apache.commons.io.FileUtils;
+import org.apache.tika.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
@@ -14,7 +15,17 @@ import uk.ac.kcl.model.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
@@ -49,17 +60,70 @@ public class PDFFileItemWriter implements ItemWriter<Document> {
             case "application/pdf":
                 handlePdf(doc);
                 break;
+            case "application/msword":
+                handleMSWord(doc);
+                break;
             default:
                 break;
             }
         }
     }
 
-    private void handlePdf(Document doc) throws IOException{
+    private void handlePdf(Document doc) throws IOException {
         FileUtils.writeByteArrayToFile(
             new File(outputPath + File.separator + doc.getDocName() + ".pdf"),
             doc.getBinaryContent()
             );
     }
 
+    private void handleMSWord(Document doc) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        // Create a temp directory for each input document
+        Path tempPath = Files.createTempDirectory(doc.getDocName());
+
+        // Dump the MS Word content to a file in the temp directory
+        File tempInputFile = new File(tempPath + File.separator + "file.doc");
+        FileUtils.writeByteArrayToFile(tempInputFile, doc.getBinaryContent());
+
+        String[] cmd = { getLibreOfficeProg(), "--convert-to", "pdf",
+                         tempInputFile.getAbsolutePath(), "--headless",
+                         "--outdir", tempPath.toString()};
+        Process process = new ProcessBuilder(cmd).start();
+        IOUtils.closeQuietly(process.getOutputStream());
+        InputStream processInputStream = process.getInputStream();
+        logStream(processInputStream);
+        FutureTask<Integer> waitTask = new FutureTask<>(process::waitFor);
+        Thread waitThread = new Thread(waitTask);
+        waitThread.start();
+        waitTask.get(30, TimeUnit.SECONDS);
+
+        // Move the file to the configured output path
+        Path tempOutputFile = tempPath.resolve("file.pdf");
+        Path outputFile = Paths.get(outputPath, doc.getDocName() + ".pdf");
+        Files.move(tempOutputFile, outputFile);
+    }
+
+    private String getLibreOfficeProg() {
+        return System.getProperty("os.name").startsWith("Windows") ? "soffice.exe" : "soffice";
+    }
+
+    private void logStream(final InputStream stream) {
+        new Thread() {
+            public void run() {
+                Reader reader = new InputStreamReader(stream, IOUtils.UTF_8);
+                StringBuilder out = new StringBuilder();
+                char[] buffer = new char[1024];
+                try {
+                    for (int n = reader.read(buffer); n != -1; n = reader.read(buffer)) {
+                        out.append(buffer, 0, n);
+                    }
+                } catch (Exception e) {
+                    LOG.error(e.getMessage());
+                } finally {
+                    IOUtils.closeQuietly(stream);
+                    IOUtils.closeQuietly(reader);
+                }
+                LOG.debug(out.toString());
+            }
+        }.start();
+    }
 }
