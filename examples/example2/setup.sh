b703467ac64519e02dd75c3616f2c91101991bb9
Lucas <lucas.roguski@gmail.com>
2018-07-16 11:13:15 +0100

    minor fixes in setup.sh scripts (typo + gen .htpasswd)

--- examples/example2/setup.sh
+++ examples/example2/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 
 COMMON_DIR="../docker-common"
-COMMMON_OUT_DIR="docker/__common"
+COMMON_OUT_DIR="docker/__common"
 
 COGSTACK_OUT_DIR="docker/__cogstack"
 
@@ -12,18 +12,19 @@ COGSTACK_OUT_DIR="docker/__cogstack"
 #
 echo "Copying the configuration files for the common docker images"
 
-if [ -e $COMMMON_OUT_DIR ]; then
-	rm -r $COMMMON_OUT_DIR;
+if [ -e $COMMON_OUT_DIR ]; then
+	rm -r $COMMON_OUT_DIR;
 fi
 
-mkdir $COMMMON_OUT_DIR
-cp -r $COMMON_DIR/* $COMMMON_OUT_DIR/
+mkdir $COMMON_OUT_DIR
+cp -r $COMMON_DIR/* $COMMON_OUT_DIR/
 
 
 # setup the common containers
 #
-if [ ! -e $COMMMON_OUT_DIR/nginx/auth/.htpasswd ]; then
+if [ ! -e $COMMON_OUT_DIR/nginx/auth/.htpasswd ]; then
 	echo "Generating user:password --> 'test:test' for nginx proxy"
+	mkdir $COMMON_OUT_DIR/nginx/auth
 	htpasswd -b -c ./nginx/auth/.htpasswd 'test' 'test'
 fi
 
