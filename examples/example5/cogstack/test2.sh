bff79731fc0ff2e11ca98eb44fa18b89d8c11ea5
Lucas <lucas.roguski@gmail.com>
2018-08-06 09:15:26 +0100

    minor refactoring + fix for using dockerhub vrrsion of cogstack image

--- examples/example5/cogstack/test2.sh
+++ examples/example5/cogstack/test2.sh
@@ -9,13 +9,20 @@ echo "*** Starting CogStack service ***"
 
 cog_start=`date +%s`
 
+#COG_PATH=/cogstack
+COG_PATH=/usr/src/docker-cogstack/cogstack
+
+#COG_CONFIG_PATH=$COG_PATH/conf
+COG_CONFIG_PATH=$COG_PATH/cogstack_conf
+
+
 #time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} ${2}"
 
-time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} /cogstack/conf/step-1/"
+#time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} /cogstack/conf/step-1/"
+#time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} /cogstack/conf/step-2/"
 
-time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} /cogstack/conf/step-2/"
-
-#/usr/bin/time -v sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} ${2}"
+time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} $COG_CONFIG_PATH/step-1/"
+time sh -c "java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} $COG_CONFIG_PATH/step-2/"
 
 cog_end=`date +%s`
 
@@ -28,6 +35,6 @@ runtime_h=$((runtime_m/60))
 echo "Duration: $runtime_h h $((runtime_m-(runtime_h*60))) m $((runtime_s-(runtime_m*60))) s"
 echo "Total: $runtime_s sec"
 
-TIME_TO_SLEEP_S=600
-echo "And now, going to sleep $TIME_TO_SLEEP_S s..."
-sleep $TIME_TO_SLEEP_S
+#TIME_TO_SLEEP_S=600
+#echo "And now, going to sleep $TIME_TO_SLEEP_S s..."
+#sleep $TIME_TO_SLEEP_S
