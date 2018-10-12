b82abddf3a8c0a3db5e63a03cb4cdb02815ea9fd
lrog <lucas.roguski@gmail.com>
2018-10-12 13:37:13 +0100

    decoupled scheduler property responsibility; added execution.mode and execution.instanceType properties to handle master-slave execution mdoe configuration

--- src/main/java/uk/ac/kcl/Main.java
+++ src/main/java/uk/ac/kcl/Main.java
@@ -50,6 +50,8 @@ public class Main {
                     try {
                         input = new FileInputStream(listOfFile);
                         properties.load(input);
+
+                        // TODO: need a proper way to validate input properties specified by user
                         if(properties.getProperty("globalSocketTimeout")!=null){
                             TcpHelper.setSocketTimeout(Integer.valueOf(properties.getProperty("globalSocketTimeout")));
                         }
@@ -60,34 +62,60 @@ public class Main {
                         ConfigurableEnvironment environment = new StandardEnvironment();
                         MutablePropertySources propertySources = environment.getPropertySources();
                         propertySources.addFirst(new MapPropertySource(listOfFile.getName(), map));
-                        @SuppressWarnings("resource")
-                        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-                        ctx.registerShutdownHook();
-                        ctx.setEnvironment(environment);
-                        String scheduling;
-                        try {
-                            scheduling = properties.getProperty("scheduler.useScheduling", "false");
-                            if (scheduling.equalsIgnoreCase("true")) {
-                                ctx.register(ScheduledJobLauncher.class);
-                                ctx.refresh();
-                            } else if (scheduling.equalsIgnoreCase("false")) {
-                                ctx.register(SingleJobLauncher.class);
-                                ctx.refresh();
-                                SingleJobLauncher launcher = ctx.getBean(SingleJobLauncher.class);
-                                launcher.launchJob();
-                            } else if (scheduling.equalsIgnoreCase("slave")) {
-                                ctx.register(JobConfiguration.class);
-                                ctx.refresh();
-                            }                        else{
-                                throw new RuntimeException("useScheduling not configured. Must be true, false or slave");
-                            }
-                        } catch (NullPointerException ex) {
-                            throw new RuntimeException("useScheduling not configured. Must be true, false or slave");
-                        }
-                    } catch (IOException e) {
+
+                        setUpApplicationContext(environment, properties);
+
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
+            }
+        }
+    }
+
+    public static void setUpApplicationContext(ConfigurableEnvironment environment, Properties properties) {
+        @SuppressWarnings("resource")
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.registerShutdownHook();
+        ctx.setEnvironment(environment);
+
+        // TODO: need a proper way to validate input properties specified by user
+        String executionMode = environment.getProperty("execution.mode", "local").toLowerCase();
+        if (executionMode != "local" && executionMode != "remote")
+            throw new RuntimeException("Invalid execution mode specified. Must be `local` (default) or `remote`.");
+
+        String instanceType = "";
+        if (executionMode == "remote")
+        {
+            if (!environment.containsProperty("execution.instanceType")) {
+                throw new RuntimeException("Instance type in remote execution not specified. Must be `master` or `slave`.");
+            }
+
+            instanceType = environment.getRequiredProperty("execution.instanceType").toLowerCase();
+            if (instanceType != "master" && instanceType != "slave")
+                throw new RuntimeException("Invalid instance type in remote execution mode specified. Must be `master` or `slave`.");
+        }
+
+        boolean useScheduling;
+        try {
+            useScheduling = Boolean.parseBoolean(environment.getProperty("scheduler.useScheduling", "false"));
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid scheduling option specified. Must be `true` or `false` (default).");
+        }
+
+        // set appropriate job configuration
+        if (executionMode == "remote" && instanceType == "slave") {
+            ctx.register(JobConfiguration.class);
+            ctx.refresh();
+        } else { // execution mode local or remote with master
+            if (useScheduling) {
+                ctx.register(ScheduledJobLauncher.class);
+                ctx.refresh();
+            } else {
+                ctx.register(SingleJobLauncher.class);
+                ctx.refresh();
+                SingleJobLauncher launcher = ctx.getBean(SingleJobLauncher.class);
+                launcher.launchJob();
             }
         }
     }
