d3ddd65faf2ebdc471695a933ecaf8f9874228ca
lrog <lucas.roguski@gmail.com>
2018-11-16 12:05:58 +0000

    minor cleanup

--- extras/dockerfiles/cogstack/test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./wait-for-it.sh "postgres:5432" "--timeout=0" "--stdout"
-./wait-for-it.sh "elasticsearch:9200" "--timeout=0" "--stdout"
-
-
-java -DLOG_FILE_NAME=$LOG_FILE_NAME -DLOG_LEVEL=$LOG_LEVEL -DFILE_LOG_LEVEL=$FILE_LOG_LEVEL -jar ${1} ${2}
