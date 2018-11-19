ae394ce37df66ffe93d1440eba60b3390775151a
lrog <lucas.roguski@gmail.com>
2018-11-19 10:13:24 +0000

    updated examples according to recentbuild refactoring; changed the names of services to be more descriptive; modified run_pipeline script to relay on SERVICES_USED variable to wait for provided services; kibana now uses ELASTICSEARCH_URL directly provided in docker-compose configuration file;

--- examples/example5/setup.sh
+++ examples/example5/setup.sh
@@ -69,7 +69,7 @@ for dt in ${doc_types[@]}; do
 	mkdir "${dp}/$COGSTACK_OUT_DIR"
 
 	cp -r cogstack/* "${dp}/$COGSTACK_OUT_DIR/"
-	#cp cogstack/test2.sh "${dp}/$COGSTACK_OUT_DIR/"
+	cp cogstack/run_pipeline.sh "${dp}/$COGSTACK_OUT_DIR/"
 
 	# copy docker files
 	#
